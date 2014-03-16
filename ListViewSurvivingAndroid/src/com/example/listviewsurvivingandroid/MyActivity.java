8cc455d6b69dac82989165c142d925792d1730fd
paracicy <paracicy@gmail.com>
2014-03-16 14:36:08 -0700

    First commit

---
+++ ListViewSurvivingAndroid/src/com/example/listviewsurvivingandroid/MyActivity.java
@@ -0,0 +1,82 @@
+package com.example.listviewsurvivingandroid;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MyActivity extends Activity {
+
+	// The data to show
+    List<Map<String, String>> planetsList = new ArrayList<Map<String,String>>();
+
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        initList();
+
+        // We get the ListView component from the layout
+        ListView lv = (ListView) findViewById(R.id.listView);
+
+
+        // This is a simple adapter that accepts as parameter
+        // Context
+        // Data list
+        // The row layout that is used during the row creation
+        // The keys used to retrieve the data
+        // The View id used to show the data. The key number and the view id must match
+        SimpleAdapter simpleAdpt = new SimpleAdapter(this, planetsList, android.R.layout.simple_list_item_1, new String[] {"planet"}, new int[] {android.R.id.text1});
+
+
+        lv.setAdapter(simpleAdpt);
+
+        // React to user clicks on item
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parentAdapter, View view, int position, long id) {
+
+                // We know the View is a TextView so we can cast it
+                TextView clickedView = (TextView) view;
+
+                Toast.makeText(MyActivity.this, "Item with id [" + id + "] - Position [" + position + "] - Planet [" + clickedView.getText() + "]", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+    }
+
+
+    private void initList() {
+        // We populate the planets
+
+        planetsList.add(createPlanet("planet", "Mercury"));
+        planetsList.add(createPlanet("planet", "Venus"));
+        planetsList.add(createPlanet("planet", "Mars"));
+        planetsList.add(createPlanet("planet", "Jupiter"));
+        planetsList.add(createPlanet("planet", "Saturn"));
+        planetsList.add(createPlanet("planet", "Uranus"));
+        planetsList.add(createPlanet("planet", "Neptune"));
+
+
+    }
+
+    private HashMap<String, String> createPlanet(String key, String name) {
+        HashMap<String, String> planet = new HashMap<String, String>();
+        planet.put(key, name);
+
+        return planet;
+    }
+
+}
